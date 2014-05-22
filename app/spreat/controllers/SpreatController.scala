627b92e3a53ff6e8e97f4547f1171ec559eab9ce
Oliver Ruebenacker <oliver@relomics.com>
2014-05-22 16:40:00 -0400

    "Hello World!", now by template.

--- app/spreat/controllers/SpreatController.scala
+++ app/spreat/controllers/SpreatController.scala
@@ -2,11 +2,12 @@ package spreat.controllers
 
 import play.api.mvc.Controller
 import play.api.mvc.Action
+import spreat.views.html.main
 
 object SpreatController extends Controller {
 
   def index = Action { implicit request =>
-    Ok("Hello World!")
+    Ok(main("Hello World!"))
   }
 
 }
