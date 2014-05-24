a56b379e827f3677c499239d27140be08ac59940
Oliver Ruebenacker <oliver@relomics.com>
2014-05-24 16:11:59 -0400

    Create circles using D3 data binding.

--- public/javascripts/spreat.js
+++ public/javascripts/spreat.js
@@ -2,18 +2,17 @@
  * 
  */
 
-function drawCircle() {
-	d3.select("svg").append("circle").attr("cx", "50").attr("cy", "50").attr("r", "40")
-	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
-	.attr("onclick", "makeBlue(this)")
-	d3.select("svg").append("circle").attr("cx", "150").attr("cy", "50").attr("r", "40")
-	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
-	.attr("onclick", "makeBlue(this)")
-	d3.select("svg").append("circle").attr("cx", "250").attr("cy", "50").attr("r", "40")
-	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
-	.attr("onclick", "makeBlue(this)")
+function drawCircles() {
+	var xs = [ 50, 150, 250 ]
+	d3.select("svg").selectAll("circle").data(xs).enter().append("circle")
+			.attr("cx", function(d) {
+				return d
+			}).attr("cy", "50").attr("r", "40").attr("stroke", "green").attr(
+					"stroke-width", "4").attr("fill", "yellow").attr("onclick",
+					"makeBlue(this)")
 }
 
 function makeBlue(element) {
-	d3.select(element).transition().attr("fill", "blue").attr("stroke", "black")
+	d3.select(element).transition().attr("fill", "blue")
+			.attr("stroke", "black")
 }
