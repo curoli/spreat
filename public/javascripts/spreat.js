0ba7ec9c096f720035272a75100eb0834379ce55
Oliver Ruebenacker <oliver@relomics.com>
2014-05-24 15:45:54 -0400

    Multiple clickeable field.

--- public/javascripts/spreat.js
+++ public/javascripts/spreat.js
@@ -5,9 +5,15 @@
 function drawCircle() {
 	d3.select("svg").append("circle").attr("cx", "50").attr("cy", "50").attr("r", "40")
 	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
-	.attr("onclick", "makeBlue()")
+	.attr("onclick", "makeBlue(this)")
+	d3.select("svg").append("circle").attr("cx", "150").attr("cy", "50").attr("r", "40")
+	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
+	.attr("onclick", "makeBlue(this)")
+	d3.select("svg").append("circle").attr("cx", "250").attr("cy", "50").attr("r", "40")
+	.attr("stroke", "green").attr("stroke-width", "4").attr("fill", "yellow")
+	.attr("onclick", "makeBlue(this)")
 }
 
-function makeBlue() {
-	d3.select("circle").attr("fill", "blue").attr("stroke", "black")
+function makeBlue(element) {
+	d3.select(element).attr("fill", "blue").attr("stroke", "black")
 }
