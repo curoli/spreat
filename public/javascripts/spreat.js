a6ad5e27442aeff69fe0b0db2679d0eab2b4d0d4
Oliver Ruebenacker <oliver@relomics.com>
2014-05-24 18:40:12 -0400

    Multiple rows of circles, hard-coded coordinates.

--- public/javascripts/spreat.js
+++ public/javascripts/spreat.js
@@ -3,12 +3,29 @@
  */
 
 function drawCircles() {
-	var xs = [ 50, 150, 250 ]
-	d3.select("svg").selectAll("circle").data(xs).enter().append("circle").transition()
-			.attr("cx", function(d) {
-				return d
-			}).attr("cy", "50").attr("r", "40").attr("stroke", "green").attr(
-					"stroke-width", "4").attr("fill", "yellow").attr("onclick",
+	var xs = [ {
+		x : 50,
+		y : 50
+	}, {
+		x : 100,
+		y : 150
+	}, {
+		x : 150,
+		y : 50
+	}, {
+		x : 200,
+		y : 150
+	}, {
+		x : 250,
+		y : 50
+	} ]
+	d3.select("svg").selectAll("circle").data(xs).enter().append("circle")
+			.transition().attr("cx", function(d) {
+				return d.x
+			}).attr("cy", function(d) {
+				return d.y
+			}).attr("r", "40").attr("stroke", "green")
+			.attr("stroke-width", "4").attr("fill", "yellow").attr("onclick",
 					"makeBlue(this)")
 }
 
