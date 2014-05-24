b82665441980f27553b5b5c79c765730f3949597
Oliver Ruebenacker <oliver@relomics.com>
2014-05-24 19:13:37 -0400

    Multiple rows of circles, computed coordinates.

--- public/javascripts/spreat.js
+++ public/javascripts/spreat.js
@@ -2,31 +2,27 @@
  * 
  */
 
+var pad = 10
+var rx = 50
+var ry = 0.5 * rx * Math.sqrt(3)
+
 function drawCircles() {
-	var xs = [ {
-		x : 50,
-		y : 50
-	}, {
-		x : 100,
-		y : 150
-	}, {
-		x : 150,
-		y : 50
-	}, {
-		x : 200,
-		y : 150
-	}, {
-		x : 250,
-		y : 50
-	} ]
+	var xs = []
+	for (var ix = 0; ix < 3; ix++) {
+		for (var iy = 0; iy < 3; iy++) {
+			xs.push({
+				x : pad + rx + ix * 2 * rx + iy * rx,
+				y : pad + rx + iy * 2 * ry
+			})
+		}
+	}
 	d3.select("svg").selectAll("circle").data(xs).enter().append("circle")
 			.transition().attr("cx", function(d) {
 				return d.x
 			}).attr("cy", function(d) {
 				return d.y
-			}).attr("r", "40").attr("stroke", "green")
-			.attr("stroke-width", "4").attr("fill", "yellow").attr("onclick",
-					"makeBlue(this)")
+			}).attr("r", rx).attr("stroke", "green").attr("stroke-width", "4")
+			.attr("fill", "yellow").attr("onclick", "makeBlue(this)")
 }
 
 function makeBlue(element) {
