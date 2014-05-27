a878c3f66b1ab0986454ef0b0e31d29071ea55c4
Oliver Ruebenacker <oliver@relomics.com>
2014-05-26 20:36:26 -0400

    Make board a hexagon consisting by default of 91 smaller hexagons.

--- public/javascripts/spreat.js
+++ public/javascripts/spreat.js
@@ -2,37 +2,43 @@
  * 
  */
 
+var boardSize = 5
 var pad = 10
-var rx = 50
-var ry = (2/3) * rx * Math.sqrt(3)
+var rx = 25
+var ry = (2 / 3) * rx * Math.sqrt(3)
+var fields = []
 
 function hexagonPoints(cxy) {
 	var xyStrings = []
-	for(var ic = 0; ic < 6; ic++) {
-		var angle = (2*ic + 1)*Math.PI/6
-		xyStrings.push((cxy.x + ry*Math.cos(angle)) + "," + (cxy.y + ry*Math.sin(angle)))
+	for (var ic = 0; ic < 6; ic++) {
+		var angle = (2 * ic + 1) * Math.PI / 6
+		xyStrings.push((cxy.x + ry * Math.cos(angle)) + ","
+				+ (cxy.y + ry * Math.sin(angle)))
 	}
 	return xyStrings.join(" ")
 }
 
 function drawBoard() {
-	var xs = []
-	for (var ix = 0; ix < 3; ix++) {
-		for (var iy = 0; iy < 3; iy++) {
-			xs.push({
-				x : pad + rx + ix * 2 * rx + iy * rx,
-				y : pad + rx + iy * 1.5 * ry
-			})
+	for (var ix = 0; ix < 2 * boardSize + 1; ix++) {
+		for (var iy = 0; iy < 2 * boardSize + 1; iy++) {
+			if (ix + iy >= boardSize && ix + iy <= 3 * boardSize) {
+				fields.push({
+					x : pad + rx + (2 * ix - boardSize) * rx + iy * rx,
+					y : pad + rx + iy * 1.5 * ry
+				})
+			}
 		}
 	}
-	d3.select("svg").selectAll("polygon").data(xs).enter().append("polygon")
-			.transition().attr("points", function(d) {
+	d3.select("#numberOfFields").text("Number of fields: " + fields.length)
+	d3.select("svg").attr("width", 2 * pad + rx * (4 * boardSize + 2)).attr(
+			"height", (2 * pad + rx + ry * (3 * boardSize + 1.5))).selectAll(
+			"polygon").data(fields).enter().append("polygon").transition()
+			.attr("points", function(d) {
 				return hexagonPoints(d)
-			}).attr("stroke", "green").attr("stroke-width", "4").attr("fill",
+			}).attr("stroke", "green").attr("stroke-width", "3").attr("fill",
 					"yellow").attr("onclick", "makeBlue(this)")
 }
 
 function makeBlue(element) {
 	d3.select(element).transition().attr("fill", "blue")
-			.attr("stroke", "black")
 }
